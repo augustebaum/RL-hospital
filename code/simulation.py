098fc941cef99ac45c67c4751233bede241c886b
Todor <todortcc@gmail.com>
2020-03-29 12:15:48 +0300

    change in 2 funcs

--- code/simulation.py
+++ code/simulation.py
@@ -5,10 +5,12 @@ from hospital import *
 # One of level 0 that has probability of being done of 0.1
 # One of level 1 that has probability of being done of 0.1
 # One of level 2 that has probability of being done of 0.05
-doctors = [Doctor(0, 0.2),
+doctors = [Doctor(0, 0.1),
            Doctor(0, 0.1),
            Doctor(1, 0.1),
-           Doctor(2, 0.05)]
+           Doctor(2, 0.05),
+           Doctor(2, 0.1)]
+
 types = [2, 1, 1]
 probs = [0.2, 0.1, 0.1, 0.05]
 
@@ -17,4 +19,5 @@ probs = [0.2, 0.1, 0.1, 0.05]
 # Patient of type 1 twice as likely than of type 2
 hospital = Hospital(20, doctors, [5, 2, 1])
 
-hospital.simulate(hospital.policy_random)
+hospital.simulate(sample_from_epsilon_greedy, episodes = 300,limit = 40)
+hospital.simulate(hospital.policy_random, episodes = 270,limit = 40)
