6040ab7cdd981f3524bd5548797e85b04feea4ed
manthflan <auguste.apple@gmail.com>
2020-04-27 17:37:46 +0200

    Solved plotting issue

--- code/exp_penalty.py
+++ code/exp_penalty.py
@@ -1,5 +1,12 @@
+from hospital import *
+from learning import *
 from experiments_extra_functions import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 
+""" Bar plot code (including `autolabel` function) obtained from:https://matplotlib.org/3.1.0/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
+"""
 def main(n_iter = 1):
     conditions = [(True, True), (True, False), (False,True), (False, False)]
     arraySarsa = np.empty([n_iter, 4])
@@ -15,6 +22,8 @@ def main(n_iter = 1):
 
     QLMeans = np.mean(arrayQL, axis=0)
     QLStds = np.std(arrayQL, axis=0)
+
+    return SarsaMeans, SarsaStds, QLMeans, QLStds
 
 def test_exp(algorithm, earlyRewards, capacity_penalty):
     x, *_ = test(algorithm,
@@ -36,9 +45,43 @@ def test_exp(algorithm, earlyRewards, capacity_penalty):
                  plot_type = None,
                  title1 = "(1.1)Sarsa + earlyReward + Capacity_penalty",
                  title2 = "(1.1)Reward evolution for the picture above",
-                 earlyRewards = earlyRewards, capacity_penalty = capacity_penalty)
+                 earlyRewards = earlyRewards,
+                 capacity_penalty = capacity_penalty)
     return misalloc(x)
+
+def autolabel(rects, xpos='center'):
+ 
+    ha = {'center': 'center', 'right': 'left', 'left': 'right'}
+    offset = {'center': 0, 'right': 1, 'left': -1}
+
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height, '.0g'),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(offset[xpos]*3, 3),  # use 3 points offset
+                    textcoords="offset points",  # in both directions
+                    ha=ha[xpos], va='bottom')
 
 
 if __name__ == '__main__':
-    main(n_iter = 5)
+    SarsaMeans, SarsaStds, QLMeans, QLStds = main(n_iter = 3)
+
+    fig, ax = plt.subplots()
+
+    ind = np.arange(4)    # the x locations for the groups
+    width = 0.3         # the width of the bars
+    p1 = ax.bar(ind, SarsaMeans, width, yerr=SarsaStds, label='SARSA')
+    p2 = ax.bar(ind + width, QLMeans, width, yerr=QLStds, label='Q-Learning')
+
+    ax.set_ylabel('Frequency Rate')
+    ax.set_title('Frequency rate of misallocation ')
+    ax.set_xticks(ind + width / 2)
+    ax.set_xticklabels(('TT', 'TF', 'FT', 'FF'))
+    ax.legend()
+
+
+    autolabel(p1, "left")
+    autolabel(p2, "right")
+
+    plt.show()
+
