f615fdab3b1ea63029637ba2c24023da2b30be29
manthflan <auguste.apple@gmail.com>
2020-04-30 15:51:50 +0200

    Added data-saving mechanic and changed plot data from mean and std dev to quantiles.

--- code/fastdoc_exp.py
+++ code/fastdoc_exp.py
@@ -1,146 +1,205 @@
 from hospital import *
 from learning import *
-from experiments_extra_functions import *
 import numpy as np
 import matplotlib.pyplot as plt
+
+from tikzplotlib import save as tikz_save
+
+# For detecting the extra arguments
+import sys
+
 # For formatting the text using tex (used in report)
-# from matplotlib import rc
-# rc('text', usetex=True)
+from matplotlib import rc
+
+rc("text", usetex=True)
 # For exporting variables to file
 from datetime import datetime
+
 """ 
-The current experiment will focus on a hospital object with 4 different types 
-of doctors and varying probability of each type of patient to arrive. 
-The goal here is to understand how the agent's behaviour changes as model parameters are changed.
-Out of the 4 doctors, doctor 3 (the most highly skilled) also happens to be highly efficient compared to the others (treats patients faster).
-This means that it could be advantageous for the agent to give a large proportion of patients to this doctor, even if they have low priority.
-However, if there are many high priority patients (who cannot be treated by anyone else), giving doctor 3 many low priority patients increases the waiting time of priority 3 patients, which is more costly that for low priority patients.
-Hence, the agent should allocate to more low skill doctors.
+You can run this file as is:
+``` python3 fastdoc_exp.py ```
+which will generate data and plot it.
+Depending on the training parameters (number_episodes, number_steps),
+this may take time.
+If all you care about is seeing the data used in the report (or any previously generated data),
+you can pass a file as an argument:
+``` python3 fastdoc_exp.py exp2/data.npz ```
+This will load the data in data.npz and plot it instead of generating new data.
+
+TODO:
+    Group data arrays into multidimensional array
+    Allow several files as input (plot data in each of them)
 """
 
-# if run directly, it generates data (takes time)
-# if run with argument, it just plots the data that was used in the report
+# Careful, all the data was generated using these probabilities so changing them will make it impossible to plot pre-exisiting data
+p_array = np.linspace(0.1, 0.9, 5)
 
 
+def main(p_array=p_array, number_tries=5):
+    if len(sys.argv) == 1:
+        data = generate(p_array, number_tries)
+    else:  # Assume only 1 extra argument: npz file containing array
+        dict = np.load(str(sys.argv[1]))
+        data = tuple(map(lambda x: x[1], dict.items()))
+        dict.close()
+
+    plot(*data, p_array)
 
 
-def main():
-    feature           = feature_7 # One-hot
-    capacity_hospital = 100
-    number_steps      = 100
-    number_episodes   = 100
-    algorithm         = sarsa
+def generate(p_array, number_tries):
+    """
+    Generates data for experiment
+    2
 
-    # p is the probability that the arriving patient is of type 3
-    p_array      = np.linspace(0.1, 0.9, 5)
-    # Number of simulations for each p (to get error)
-    number_tries = 5
+    Inputs:
+    p_array      - array of probabilities (type 3 arrivals)
+    number_tries - Number of trials per probability
+
+    Outputs:
+    Data Arrays with len(p_array) lines and number_tries columns 
+    """
+    feature = feature_12  # One-hot
+    capacity_hospital = 500
+    number_steps = 500
+    number_episodes = 50
+    algorithm = sarsa
+
     # Number of people in queue 3 for each p
-    queue3       = np.empty([number_tries, len(p_array)])
     # Number of people in queue 3 that are of type 3 for each p
-    queue3type3  = queue3
     # Average amount of time type 3 patients waited for
-    time3        = queue3
+    (queue3, queue3type3, time3) = tuple(np.empty([number_tries, len(p_array)]) for _ in range(3))
 
-    ##############################################
+    ###############################################
 
     # Efficiency of low skill doctors
     p_slow = 0.4
     # Efficiency of high skill doctor
     p_fast = 0.8
 
-    doctors_1 = [Doctor(0, p_slow),
-                 Doctor(1, p_slow),
-                 Doctor(2, p_slow),
-                 Doctor(3, p_fast)]
+    doctors = [
+        Doctor(0, p_slow),
+        Doctor(1, p_slow),
+        Doctor(2, p_slow),
+        Doctor(3, p_fast),
+    ]
 
     ##############################################
 
     for j, p in enumerate(p_array):
-        arrival_rates = [1, 1, 1, round(3*p/(1-0.99999*p))]
+        arrival_rates = [1, 1, 1, round(3 * p / (1 - 0.99999 * p))]
 
         # this hospital object used only to calculate the random rewards list
         # Not very useful
-        hospital_r = Hospital( capacity_hospital, doctors_1, [1, 1, 1, 1])
+        hospital_r = Hospital(capacity_hospital, doctors, [1, 1, 1, 1])
 
-        # # Random policy (total_reward_per_episode_r is needed in `test`)
-        t_list_r, Q_optimal_weights_r, total_reward_per_episode_r =\
-            algorithm(
-                hospital_r,
-                feature,
-                0,
-                0,
-                1,
-                number_episodes,
-                number_steps)
+        # Random policy (total_reward_per_episode_r is needed in `test`)
+        t_list_r, Q_optimal_weights_r, total_reward_per_episode_r = algorithm(
+            hospital_r, feature, 0, 0, 1, number_episodes, number_steps
+        )
 
         # Run hospital with the naive policy for number_steps steps
         # Record allocations and plot heatmap
         # p_naive, r_naives = simulate( hospital_r, naive = True, steps = number_steps, plot = "map")
 
         # Train, simulate and gather:
-        # - The number of non-type 3 patients in queue 3
+        # - The number of patients in queue 3
+        # - The number of patients of type 3 in queue 3
         # - The time that 3-patients waited
 
         for i in range(number_tries):
-        # Testing using equal arrival probabilities to provide unbiased account
-            props, rewards, cured, time_array, cured_types =\
-                test(
-                    sarsa,
-                    capacity_hospital,
-                    number_steps,
-                    number_episodes,
-                    arrival_rates,
-                    doctors_1,
-                    feature,
-                    total_reward_per_episode_r,
-                    p_prob_test = [1, 1, 1, 1],
-                    gamma = 0.9,
-                    alpha = None,
-                    epsilon = 0.1,
-                    plot_type = None,
-                    title1 = "Type 3 patients arrive {:.0%} of the time during training".format(p),
-                    title2 = "Reward evolution for the picture above")
+            # Testing using equal arrival probabilities to provide unbiased account
+            props, rewards, cured, time_array, cured_types = test(
+                algorithm,
+                capacity_hospital,
+                number_steps,
+                number_episodes,
+                arrival_rates,
+                doctors,
+                feature,
+                p_prob_test=[1, 1, 1, 1],
+                gamma=0.9,
+                alpha=None,
+                epsilon=0.1,
+                plot_type=None,
+                title1="Type 3 patients arrive {:.0%} of the time during training".format(p),
+                title2="Reward evolution for the picture above",
+            )
 
-            queue3[i, j]      = sum(props[:,3])
-            queue3type3[i, j] = props[3,3]
-            time3[i, j]       = np.mean(time_array[3])
+            queue3[i, j] = sum(props[:, 3])
+            queue3type3[i, j] = props[3, 3]
+            time3[i, j] = np.mean(time_array[3]) if time_array[3] else 0
+
+    # Save to file for further analysis
+    np.savez(
+        "exp2/feature_12--"
+        + str(number_episodes)
+        + "episodes"
+        + str(number_steps)
+        + "steps"
+        + datetime.now().strftime("%H-%M-%S"),
+        queue3,
+        queue3type3,
+        time3,
+    )
 
     return queue3, queue3type3, time3
 
-    # Save to file for further analysis
-    # np.savez("exp2_data_"+datetime.now().strftime("%d-%m,%H:%M"), queue3, queue3type3)
 
-def plot(queue3, queue3type3, time3):
-    av_q3    = np.mean(queue3, axis=0)
-    av_q3t3  = np.mean(queue3type3, axis=0)
-    av_time3 = np.mean(time3, axis=0)
+def errors(arr):
+    """
+    Distance from median to 20th and 70th percentile, for error bars
+    """
+    a = np.median(arr, axis=0)
+    return a, np.abs(np.quantile(arr, [0.20, 0.70], axis=0) - a)
 
-    yerr_q3    = 2*np.std(queue3, axis=0)
-    yerr_q3t3  = 2*np.std(queue3type3, axis=0)
-    yerr_time3 = 2*np.std(time3, axis=0)
 
-    print("2 std devs for queue3", yerr_q3)
-    print("2 std devs for queue3type3", yerr_q3t3)
-    print("2 std devs for time3", yerr_time3)
-
-    plt.figure()
-    # plt.errorbar(p_array, av_q3, yerr=yerr_q3, elinewidth=1, capsize=2, label="Proportion of patients in queue 3")
-    # plt.errorbar(p_array, av_q3t3, yerr=yerr_q3t3, elinewidth=1, capsize=2, label="Proportion of patients of type 3 in queue 3")
+def plot_errorbar(points_array, label):
     plt.errorbar(
         p_array,
-        av_time3,
-        yerr=yerr_time3,
+        points_array[0],
+        yerr=points_array[1],
         elinewidth=1,
         capsize=2,
-        label="Average time waited by (cured) type 3 patients")
+        label=label,
+    )
+
+
+def plot(queue3, queue3type3, time3, p_array):
+    # Could be made more extensiible with multidimensional array
+    # av_q3 = np.mean(queue3, axis=0)
+    # av_q3t3 = np.mean(queue3type3, axis=0)
+    # av_time3 = np.mean(time3, axis=0)
+
+    # yerr_q3 = 2 * np.std(queue3, axis=0)
+    # yerr_q3t3 = 2 * np.std(queue3type3, axis=0)
+    # yerr_time3 = 2 * np.std(time3, axis=0)
+
+    # print("2 std devs for queue3", yerr_q3)
+    # print("2 std devs for queue3type3", yerr_q3t3)
+    # print("2 std devs for time3", yerr_time3)
+
+    plt.figure(1)
+    plot_errorbar(errors(queue3), "Proportion of patients in queue 3")
+    plot_errorbar(errors(queue3type3), "Proportion of patients of type 3 in queue 3")
+    plt.axis([0, 1, -1, 101])
     plt.legend()
     plt.xlabel("Probability that arriving patient has type 3 during training")
+    plt.tight_layout()
+
+    plt.figure(2)
+    plot_errorbar(errors(time3), "Average time waited by (cured) type 3 patients")
+    plt.axis([0, 1, -0.05, plt.axis()[3] + 0.05])
+    plt.legend()
+    plt.xlabel("Probability that arriving patient has type 3 during training")
+    plt.tight_layout()
 
     plt.show()
+    # tikz_save('test.tex')
 
 
-if __name__=='__main__':
-    main()
+if __name__ == "__main__":
+    main(number_tries=2)
+
+
 
 
--- code/rewards_exp.py
+++ code/rewards_exp.py
@@ -1,88 +1,147 @@
 from hospital import *
 from learning import *
+from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
-# import matplotlib
+import sys
+
 
 """ Bar plot code (including `autolabel` function) obtained from:https://matplotlib.org/3.1.0/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
 """
 
-def main(n_iter = 1, number_steps = 100, number_episodes = 100):
-    conditions = [(True, True), (True, False), (False,True), (False, False)]
+conditions = [(True, True), (True, False), (False, True), (False, False)]
+
+
+def main(number_tries=5, number_episodes=100, number_steps=100):
+    if len(sys.argv) == 1:
+        data = generate(number_tries, number_steps, number_episodes)
+    else:  # Assume only 1 extra argument: npz file containing array
+        dict = np.load(str(sys.argv[1]))
+        data = tuple(map(lambda x: x[1], dict.items()))
+        dict.close()
+
+    plot(*data)
+
+
+def generate(n_iter, number_steps, number_episodes):
     arraySarsa = np.empty([n_iter, 4])
     arrayQL = np.empty([n_iter, 4])
 
     for i in range(0, n_iter):
         for j, x in enumerate(conditions):
-            arraySarsa[i, j] = test_exp(sarsa, *x, number_episodes = number_episodes, number_steps = number_steps)
-            arrayQL[i, j]    = test_exp(ql, *x, number_episodes = number_episodes, number_steps = number_steps)
+            arraySarsa[i, j] = test_exp(
+                sarsa, *x, number_episodes=number_episodes, number_steps=number_steps
+            )
+            arrayQL[i, j] = test_exp(
+                ql, *x, number_episodes=number_episodes, number_steps=number_steps
+            )
 
-    SarsaMeans = np.mean(arraySarsa, axis=0)
-    SarsaStds = np.std(arraySarsa, axis=0)
+    # Save to file for further analysis
+    np.savez(
+        "exp3/exp3_"
+        + str(number_episodes)
+        + "episodes_"
+        + str(number_steps)
+        + "steps_"
+        + datetime.now().strftime("%H-%M-%S"),
+        arraySarsa,
+        arrayQL,
+    )
 
-    QLMeans = np.mean(arrayQL, axis=0)
-    QLStds = np.std(arrayQL, axis=0)
+    return arraySarsa, arrayQL
+    # return SarsaMeans, SarsaStds, QLMeans, QLStds
 
-    return SarsaMeans, SarsaStds, QLMeans, QLStds
 
-def test_exp(algorithm, earlyRewards, capacity_penalty, number_steps = 100, number_episodes = 100):
-    x, *_ = test(algorithm,
-                 capacity_hospital = 100,
-                 number_steps = number_steps,
-                 number_episodes = number_episodes,
-                 p_arr_prob = [1, 1, 1, 1, 1, 1],
-                 doctors = [Doctor(0, 0.1),
-                 Doctor(1, 0.1),
-                 Doctor(2, 0.9),
-                 Doctor(3, 0.1),
-                 Doctor(4, 0.5),
-                 Doctor(5, 0.1)],
-                 feature = feature_7,
-                 rand_rewards = 0,
-                 gamma = 0.9,
-                 alpha = None,
-                 epsilon = 0.1,
-                 plot_type = "hist",
-                 title1 = "",
-                 title2 = "",
-                 earlyRewards = earlyRewards,
-                 capacity_penalty = capacity_penalty)
+def test_exp(
+    algorithm,
+    earlyRewards,
+    capacity_penalty,
+    number_steps=100,
+    number_episodes=100
+):
+    x, *_ = test(
+        algorithm,
+        capacity_hospital=100,
+        number_steps=number_steps,
+        number_episodes=number_episodes,
+        p_arr_prob=[1, 1, 1, 1, 1, 1],
+        doctors=[
+            Doctor(0, 0.1),
+            Doctor(1, 0.1),
+            Doctor(2, 0.9),
+            Doctor(3, 0.1),
+            Doctor(4, 0.5),
+            Doctor(5, 0.1),
+        ],
+        feature=feature_7,
+        rand_rewards=0,
+        gamma=0.9,
+        alpha=None,
+        epsilon=0.1,
+        plot_type=None,
+        title1="",
+        title2="",
+        earlyRewards=earlyRewards,
+        capacity_penalty=capacity_penalty,
+    )
     return misalloc(x)
 
-def autolabel(rects, xpos='center'):
- 
-    ha = {'center': 'center', 'right': 'left', 'left': 'right'}
-    offset = {'center': 0, 'right': 1, 'left': -1}
 
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate('{}'.format(height, '.0f'),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(offset[xpos]*3, 3),  # use 3 points offset
-                    textcoords="offset points",  # in both directions
-                    ha=ha[xpos], va='bottom')
+def errors(arr):
+    """
+    Distance from median to 20th and 70th percentile, for error bars
+    """
+    a = np.median(arr, axis=0)
+    return a, np.abs(np.quantile(arr, [0.20, 0.70], axis=0) - a)
 
-if __name__ == '__main__':
-    SarsaMeans, SarsaStds, QLMeans, QLStds = main(n_iter = 2, number_steps = 50, number_episodes = 1)
 
-    print(SarsaMeans, SarsaStds, QLMeans, QLStds)
+def plot(arraySarsa, arrayQL):
+    # SarsaMeans = np.mean(arraySarsa, axis=0)
+    # SarsaStds = np.std(arraySarsa, axis=0)
+
+    # QLMeans = np.mean(arrayQL, axis=0)
+    # QLStds = np.std(arrayQL, axis=0)
+
+    Sarsa_points = errors(arraySarsa)
+    QL_points = errors(arrayQL)
 
     fig, ax = plt.subplots()
 
-    ind = np.arange(4)    # the x locations for the groups
-    width = 0.3         # the width of the bars
+    ind = np.arange(4)  # the x locations for the groups
+    width = 0.3  # the width of the bars
 
-    p1 = ax.bar(ind, SarsaMeans, width, yerr=SarsaStds, label='SARSA')
-    p2 = ax.bar(ind + width, QLMeans, width, yerr=QLStds, label='Q-Learning')
+    p1 = ax.bar(ind, Sarsa_points[0], width, yerr=Sarsa_points[1], label="SARSA")
+    p2 = ax.bar(ind + width, QL_points[0], width, yerr=QL_points[1], label="Q-learning")
+    # p2 = ax.bar(ind + width, QLMeans, width, yerr=QLStds, label="Q-Learning")
 
-    ax.set_ylabel('Frequency Rate')
-    ax.set_title('Frequency rate of misallocation')
+    ax.set_ylabel("Frequency Rate")
+    ax.set_title("Frequency rate of misallocation")
     ax.set_xticks(ind + width / 2)
-    ax.set_xticklabels(('TT', 'TF', 'FT', 'FF'))
+    ax.set_xticklabels(("TT", "TF", "FT", "FF"))
     ax.legend()
 
-    autolabel(p1, "left")
-    autolabel(p2, "right")
+    autolabel(p1, ax, "left")
+    autolabel(p2, ax, "right")
 
     plt.show()
 
+
+def autolabel(rects, ax, xpos="center"):
+
+    ha = {"center": "center", "right": "left", "left": "right"}
+    offset = {"center": 0, "right": 1, "left": -1}
+
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate(
+            "{}".format(height, ".0f"),
+            xy=(rect.get_x() + rect.get_width() / 2, height),
+            xytext=(offset[xpos] * 3, 3),  # use 3 points offset
+            textcoords="offset points",  # in both directions
+            ha=ha[xpos],
+            va="bottom",
+        )
+
+
+if __name__ == "__main__":
+    main(number_tries=5, number_episodes=50, number_steps=100)
