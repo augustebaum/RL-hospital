7870b7d82d2f7016ea5f94c7c165582fa3976201
manthflan <auguste.apple@gmail.com>
2020-05-04 13:22:20 +0200

    gathers data about misallocations as well as waiting times

--- code/rewards_exp.py
+++ code/rewards_exp.py
@@ -21,25 +21,24 @@ def main(number_tries=5, number_episodes=100, number_steps=100):
         data = tuple(map(lambda x: x[1], dict.items()))
         dict.close()
 
-	# Just in case there are NaN
-    # for arr in data:
-        # arr = arr[~np.isnan(arr).any(axis=1)]
-
+    # Just in case there are NaN
     data = map(lambda arr: arr[~np.isnan(arr).any(axis=1)], data)
 
     plot(*data)
 
 
 def generate(n_iter, number_steps, number_episodes):
-    arraySarsa = np.empty([n_iter, 4])
-    arrayQL = np.empty([n_iter, 4])
+    arraySarsa_misalloc = np.empty([n_iter, 4])
+    arrayQL_misalloc = np.empty([n_iter, 4])
+    arraySarsa_time = np.empty([n_iter, 4])
+    arrayQL_time = np.empty([n_iter, 4])
 
     for i in range(0, n_iter):
         for j, x in enumerate(conditions):
-            arraySarsa[i, j] = test_exp(
+            (arraySarsa_misalloc[i, j], arraySarsa_time[i, j]) = test_exp(
                 sarsa, *x, number_episodes=number_episodes, number_steps=number_steps
             )
-            arrayQL[i, j] = test_exp(
+            (arrayQL_misalloc[i, j], arrayQL_time[i, j]) = test_exp(
                 ql, *x, number_episodes=number_episodes, number_steps=number_steps
             )
 
@@ -53,12 +52,13 @@ def generate(n_iter, number_steps, number_episodes):
         + str(number_steps)
         + "steps_"
         + datetime.now().strftime("%H-%M-%S"),
-        arraySarsa,
-        arrayQL,
+        arraySarsa_misalloc,
+        arrayQL_misalloc,
+        arraySarsa_time,
+        arrayQL_time,
     )
 
-    return arraySarsa, arrayQL
-    # return SarsaMeans, SarsaStds, QLMeans, QLStds
+    return arraySarsa_misalloc, arrayQL_misalloc, arraySarsa_time, arrayQL_time
 
 
 def test_exp(
@@ -66,17 +66,17 @@ def test_exp(
     earlyRewards,
     capacity_penalty,
     number_steps=100,
-    number_episodes=100
+    number_episodes=80
 ):
-    _, _, _, times, _ = test(
+    props, _, _, times, _, _ = test(
         algorithm,
-        capacity_hospital=100,
+        capacity_hospital=40,
         number_steps=number_steps,
         number_episodes=number_episodes,
         p_arr_prob=[1, 1, 1],
         doctors=[
             Doctor(0, 0.1),
-            Doctor(1, 0.1),
+            Doctor(1, 0.4),
             Doctor(2, 0.1),
         ],
         feature=feature_7,
@@ -90,7 +90,7 @@ def test_exp(
         earlyRewards=earlyRewards,
         capacity_penalty=capacity_penalty,
     )
-    return np.median(list(map(lambda l: np.median(l) if len(l)>0 else 0, times)))
+    return misalloc(props), np.median(list(map(lambda l: np.median(l) if len(l)>0 else 0, times)))
 
 
 def errors(arr):
@@ -101,26 +101,22 @@ def errors(arr):
     return a, np.abs(np.quantile(arr, [0.20, 0.80], axis=0) - a)
 
 
-def plot(arraySarsa, arrayQL):
-    # SarsaMeans = np.mean(arraySarsa, axis=0)
-    # SarsaStds = np.std(arraySarsa, axis=0)
-
-    # QLMeans = np.mean(arrayQL, axis=0)
-    # QLStds = np.std(arrayQL, axis=0)
-
-    Sarsa_points = errors(arraySarsa)
-    QL_points = errors(arrayQL)
+def plot(arraySarsa_misalloc, arrayQL_misalloc, arraySarsa_time, arrayQL_time):
+    Sarsa_misalloc_points = errors(arraySarsa_misalloc)
+    QL_misalloc_points = errors(arrayQL_misalloc)
+    Sarsa_time_points = errors(arraySarsa_time)
+    QL_time_points = errors(arrayQL_time)
 
     fig, ax = plt.subplots()
 
     ind = np.arange(4)  # the x locations for the groups
     width = 0.3  # the width of the bars
 
-    p1 = ax.bar(ind, Sarsa_points[0], width, yerr=Sarsa_points[1], label="SARSA")
-    p2 = ax.bar(ind + width, QL_points[0], width, yerr=QL_points[1], label="Q-learning")
-    # p2 = ax.bar(ind + width, QLMeans, width, yerr=QLStds, label="Q-Learning")
+	# Misallocations
+    p1 = ax.bar(ind, Sarsa_misalloc_points[0], width, yerr=Sarsa_misalloc_points[1], label="SARSA")
+    p2 = ax.bar(ind + width, QL_misalloc_points[0], width, yerr=QL_misalloc_points[1], label="Q-learning")
 
-    ax.set_ylabel("Median waiting time")
+    ax.set_ylabel("Proportion of misallocated patients")
     # ax.set_title("Frequency rate of misallocation")
     ax.set_xticks(ind + width / 2)
     ax.set_xticklabels(("TT", "TF", "FT", "FF"))
@@ -128,6 +124,24 @@ def plot(arraySarsa, arrayQL):
 
     autolabel(p1, ax, "left")
     autolabel(p2, ax, "right")
+
+    fig_time, ax_time = plt.subplots()
+
+    ind = np.arange(4)  # the x locations for the groups
+    width = 0.3  # the width of the bars
+
+    # Misallocations
+    p1 = ax_time.bar(ind, Sarsa_time_points[0], width, yerr=Sarsa_time_points[1], label="SARSA")
+    p2 = ax_time.bar(ind + width, QL_time_points[0], width, yerr=QL_time_points[1], label="Q-learning")
+
+    ax_time.set_ylabel("Median waiting time")
+    # ax_time.set_title("Frequency rate of misallocation")
+    ax_time.set_xticks(ind + width / 2)
+    ax_time.set_xticklabels(("TT", "TF", "FT", "FF"))
+    ax_time.legend()
+
+    autolabel(p1, ax_time, "left")
+    autolabel(p2, ax_time, "right")
 
     plt.show()
 
@@ -151,4 +165,4 @@ def autolabel(rects, ax, xpos="center"):
 
 
 if __name__ == "__main__":
-    main(number_tries=1, number_episodes=50, number_steps=100)
+    main(number_tries=2, number_episodes=80, number_steps=100)
