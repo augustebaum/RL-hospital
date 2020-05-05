f938788ff4c6c932b924d89db433ff8c68ab90f3
Todor <todortcc@gmail.com>
2020-05-05 23:07:24 +0300

    feature experiment finished

--- code/feat_exp.py
+++ code/feat_exp.py
@@ -1,50 +1,9 @@
 from hospital import *
 from learning import *
-# from experiments_extra_functions import *
+from fastdoc_exp import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-##############################################################################
-##############################################################################
-##############################################################################
-"""
-Control panel for those who are currently grading this project.
-In this box there are options that will easily print out certain results
-regarding this experiment.
-Set only one of the variables below to "True" at a time.
-
-run_each_featurisation_once_and_plot -> for each featurisation there will be 2 plots
-             1st plot will show the allocation of patient after a simulation of the learned policy
-             2nd will show the rewards at each episode during the learning process
-number_of_experiments -> defines how many times the learning process is repeated for a certain feature
-                      -> Our data about mean rewards and standard deviation is gathered after 
-                         a significant number of experiments (100 for each feature) so that it is accurate.
-                      -> Note that that the runtime for 100 experiments on a certain feature might take up to 5 minutes
-number_of_experiments -> initially set to 10  -  Data in report histogram taken after 100 for each feature
-run_naive_policy      -> if a patient is of type X then they are assigned to a doctor of type X
-"""
-
-run_each_featurisation_once_and_plot = True
-
-run_feature_1  = False
-run_feature_7  = False
-run_feature_8  = False
-run_feature_9  = False
-run_feature_10 = False
-run_feature_11 = False
-run_feature_12 = False
-run_feature_13 = False
-number_of_experiments = 1
-
-run_naive_policy = False
-
-calcRewards = False
-
-
-
-##############################################################################
-##############################################################################
-##############################################################################
 
 # these are the relative probabilites of each patient arriving - the index in 
 # the list corresponds to the patient's type. If we had a list [1,2,4] then this would mean
@@ -52,9 +11,13 @@ calcRewards = False
 # any given step, patients of type 1 - probability of 2/7 and type 2 - 4/7
 p_arr_prob = [1, 1, 1, 1, 1, 1]
 
+# a list with the features to be used
+features = [feature_1, feature_7, feature_12, feature_13]
+
+featNames = {0:"feature_1", 1:"feature_7", 2:"feature12", 3:"feature_13"}
 
 # doctors_1 is currently used for all tests (it looks like a good choice
-doctors_1 = [Doctor(0, 0.1),
+doctors = [Doctor(0, 0.1),
              Doctor(1, 0.1),
              Doctor(2, 0.9),
              Doctor(3, 0.4),
@@ -66,10 +29,111 @@ number_of_steps = 100
 number_of_episodes = 100
 
 # this hospital object used only to calculate the random rewards list
-hospital_r = Hospital(capacity, doctors_1, p_arr_prob)
-# t_list_r, Q_optimal_weights_r, total_reward_per_episode_r = sarsa(hospital_r, feature_7, 0, 0, 1, number_of_episodes, number_of_steps)
+hospital_object = Hospital(capacity, doctors, p_arr_prob)
 
-def test_17(
+
+# used to create a matrix that will hold the necessary data
+n_features = 4
+
+# matrices to hold the reward data needed for the final plots
+rew_sim_matrix = [[] for _ in range(n_features)]
+rew_learning_matrix = np.zeros((n_features, number_of_episodes))
+
+
+
+# We note that foe each feature in our report there 300 data points.
+# i.e. num_exp should be 300 in order to get the same data
+# However, we do not recommend increasing the number of experiments to 
+# more than 50 -> it would take far too much time.
+##############################################################################
+Number_of_experiments = 25
+##############################################################################
+
+
+
+def main(num_exp):
+    
+    naive_median_quantiles, naive_evol = naive_policy()
+    
+    for j, feature in enumerate(features):
+        for i in range(num_exp):
+            
+            # plot only once and for feature_12 only
+            if i == 0 and j == 2:
+                plot = "hist"
+            else:
+                plot = False
+            
+            # learning and simulation function
+            learning_rew, sim_rew, *_  = test_exp(sarsa,
+                                                  capacity,
+                                                  number_of_steps,
+                                                  number_of_episodes,
+                                                  p_arr_prob, 
+                                                  doctors,
+                                                  feature,
+                                                  plot_type = plot,
+                                                  title1 = featNames[j])
+            
+            # add data to the matrix with simulation rewards
+            simulation_rewards(sum(sim_rew), j)
+            
+            # add data to the matrix with learning rewards evolution
+            learning_rewards(learning_rew, j, num_exp)
+            print("This should be the final reward: ", sum(sim_rew))
+            
+    # output results such as Figure 3 - reward evolution during learning       
+    show_learning_curves(rew_learning_matrix, naive_evol)
+    
+    # calculate the data needed for Figure 2
+    calc_median_quantiles(rew_sim_matrix)
+    
+    show_simulation_rewards(rew_sim_matrix, naive_median_quantiles)
+    
+    
+def calc_median_quantiles(rew_matrix):
+    for i in range(len(rew_matrix)):
+        rew_matrix[i] = errors(rew_matrix[i])
+    
+def simulation_rewards(rew, index):
+    rew_sim_matrix[index].append(rew)
+
+def learning_rewards(rew_per_ep, index, num_exp):
+    rew_learning_matrix[index] += np.array(rew_per_ep) / num_exp
+    
+def show_learning_curves(rew_matrix, naive_rew):
+    plt.plot(range(len(rew_matrix[0])), rew_matrix[0], "-b", label = "Feature 1")
+    plt.plot(range(len(rew_matrix[1])), rew_matrix[1], "-g", label = "Feature 7")
+    plt.plot(range(len(rew_matrix[2])), rew_matrix[2], "-r", label = "Feature 12")
+    plt.plot(range(len(rew_matrix[3])), rew_matrix[3], "-c", label = "Feature 13")
+    plt.plot(range(len(naive_rew)), naive_rew, "-m", label = "Naive policy")
+    plt.legend()
+    
+def show_simulation_rewards(rew_matrix, naive_median_quantiles):
+    fig = plt.figure()
+    ax = fig.add_axes([0,0,1,1])
+    features = ['Naive policy', 'feat1', 'feat7', 'feat13', 'feat12']
+    rewards = [naive_median_quantiles[0], rew_matrix[0][0], rew_matrix[1][0], rew_matrix[3][0], rew_matrix[2][0]]
+    quantiles = [[naive_median_quantiles[1][0], rew_matrix[0][1][0], rew_matrix[1][1][0], rew_matrix[3][1][0], rew_matrix[2][1][0] ],
+                 [naive_median_quantiles[1][1], rew_matrix[0][1][1], rew_matrix[1][1][1], rew_matrix[3][1][1], rew_matrix[2][1][1] ]]
+    ax.bar(features, rewards, yerr = quantiles)
+    plt.show()
+
+def naive_policy():
+    
+    print("Experiment 1 initiated")
+    
+    reward_list = [0 for _ in range(number_of_episodes)]
+    
+    for experiment in range(Number_of_experiments):
+        for ep in range(number_of_episodes):
+            
+            p_naive, r_naives, *rest = simulate(hospital_object, naive = True, steps = number_of_steps)
+            reward_list[ep] += (sum(r_naives)) / Number_of_experiments
+                
+    return errors(reward_list), reward_list
+            
+def test_exp(
     algorithm,
     capacity_hospital,
     number_steps,
@@ -162,8 +226,10 @@ def test_17(
                  " rewards and "+\
                  ("a" if capacity_penalty else "no")+\
                  " capacity penalty"
+                 
+    # if feature is feature_12 works
 
-    props, rewards, cured, time, cured_types = simulate(
+    props, rewards, cured, time, cured_types, size = simulate(
             hospital,
             feature,
             Q_weights,
@@ -189,54 +255,36 @@ def test_17(
     # Extra information to be printed for the first figure
     print_extra_info(rewards, cured, number_steps, cured_types, sum(map(sum, time)), title1)
     
-    return props, rewards, cured, time, cured_types , total_reward_per_episode
-
-# simple func to write to a txt file
-def writeToFile(fileLocation, rewardList):
-    myfile = open(fileLocation,"a")
-    for reward in rewardList:
-        myfile.write(str(reward) + "\n")
-    myfile.close()
-
+    return total_reward_per_episode, rewards, props, cured, time, cured_types, size
     
-def misalloc(alloc_matrix):
-    return np.sum(np.tril(alloc_matrix, -1))
+def feature_experiment(num_exp,
+                       featurisation,
+                       rew_graph = False,
+                       naive_rew = None,
+                       tit1 = "", 
+                       tit2 = ""):
 
-
-
-# sets the currently needed parameters in the "test" function 
-# to reduce the code
-def feature_experiment(num_exp, featurisation, fileLocRew = None, fileLocMis = None, fileLocEvol = None, plot = None, rew_graph = False, naive_rew = None, tit1 = "", tit2 = ""):
-    
-    exp_rewards = []
-    misalloc_list = []
-    rewards_per_ep = []
     
     for experiment in range(num_exp):
     
-        props, rewards, c, t, cr, rewperep = test_17(algorithm = sarsa,
-                                                     capacity_hospital = capacity,
-                                                     number_steps = number_of_steps,
-                                                     number_episodes = number_of_episodes,
-                                                     p_arr_prob = p_arr_prob,
-                                                     doctors = doctors_1,
-                                                     feature = featurisation,
-                                                     rand_rewards = None,
-                                                     gamma = 0.9,
-                                                     alpha = None,
-                                                     epsilon = 0.1,
-                                                     plot_type = plot,
-                                                     title1 = tit1,
-                                                     title2 = tit2,
-                                                     earlyRewards = True,
-                                                     capacity_penalty = False,
-                                                     reward_evolution = rew_graph,
-                                                     naive_rewards = naive_rew) 
+        props, rewards, c, t, cr, size = test_exp(algorithm = sarsa,
+                                                  capacity_hospital = capacity,
+                                                  number_steps = number_of_steps,
+                                                  number_episodes = number_of_episodes,
+                                                  p_arr_prob = p_arr_prob,
+                                                  doctors = doctors,
+                                                  feature = featurisation,
+                                                  rand_rewards = None,
+                                                  gamma = 0.9,
+                                                  alpha = None,
+                                                  epsilon = 0.1,
+                                                  title1 = tit1,
+                                                  title2 = tit2,
+                                                  earlyRewards = True,
+                                                  capacity_penalty = False,
+                                                  reward_evolution = rew_graph,
+                                                  naive_rewards = naive_rew) 
         
-        if len(rewards_per_ep) == 0:
-            rewards_per_ep = np.array(rewperep)
-        else:
-            rewards_per_ep += np.array(rewperep)
     
         
         exp_rewards.append(sum(rewards))
@@ -246,155 +294,8 @@ def feature_experiment(num_exp, featurisation, fileLocRew = None, fileLocMis = N
         
     rewards_per_ep = rewards_per_ep / num_exp  
     
-    # save all the reward info in a file
-    if fileLocRew != None:
-        writeToFile(fileLocRew, exp_rewards)
-    # save the info about misallocations
-    if fileLocMis != None:
-        writeToFile(fileLocMis, misalloc_list)
-    # saves rewards evolution information
-    if fileLocEvol != None:
-        writeToFile(fileLocEvol, rewards_per_ep)
-    
-    if plot == None:
-        print("\n\nThe table results are below: ")
-        print("The list with the rewards", exp_rewards)
-        print("The average reward after {} experiments is {}".format(num_exp, np.mean(exp_rewards)))
-        print("The median is {}".format(np.median(np.array(exp_rewards))))
-        print("The standard deviation is {}".format(np.std(np.array(exp_rewards))))
-    
 
+if __name__ == "__main__":
+    main(Number_of_experiments)
     
     
-
-if run_each_featurisation_once_and_plot:
-    
-    #feature_experiment(1, feature_1, plot = "hist", rew_graph = True, 
-    #                   tit1 = "feature_1 patient allocation", tit2 = "feature_1 rewards during learning")
-    #feature_experiment(1, feature_7, plot = "hist", rew_graph = True,
-    #                   tit1 = "feature_7 patient allocation", tit2 = "feature_7 rewards during learning")
-    feature_experiment(1, feature_12, plot = "hist", rew_graph = True,
-                       tit1 = "feature_12 patient allocation", tit2 = "feature_12 rewards during learning")
-
-
-elif run_feature_1:
-    feature_experiment(number_of_experiments,feature_1,
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\rewardsFeature1.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\misallocFeature1.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\evolFeature1.txt",
-                       tit1 = "feature 1 current experiment")
-
-
-elif run_feature_7:
-    feature_experiment(number_of_experiments, feature_7,
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\rewardsFeature7.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\misallocFeature7.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\evolFeature7.txt",
-                       tit1 = "feature 7 current experiment")
-
-
-elif run_feature_8:
-    feature_experiment(number_of_experiments,feature_8,
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\rewardsFeature8.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\misallocFeature8.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\evolFeature8.txt",
-                       tit1 = "feature 8 current experiment")
-
-
-elif run_feature_9:
-    feature_experiment(number_of_experiments,feature_9,
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\rewardsFeature9.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\misallocFeature9.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\evolFeature9.txt",
-                       tit1 = "feature 9 current experiment")
-    
-
-elif run_feature_10:
-    feature_experiment(number_of_experiments,feature_10,
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\rewardsFeature10.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\misallocFeature10.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\evolFeature10.txt",
-                       tit1 = "feature 10 current experiment")
-    
-
-elif run_feature_11:
-    feature_experiment(number_of_experiments,feature_11,
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\rewardsFeature11.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\misallocFeature11.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\evolFeature11.txt",
-                       tit1 = "feature 11 current experiment")
-    
-    
-elif run_feature_12:
-    feature_experiment(number_of_experiments, feature_12,
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\rewardsFeature12.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\misallocFeature12.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\evolFeature12.txt",
-                       tit1 = "feature 12 current experiment")
-
-
-elif run_feature_13:
-    feature_experiment(number_of_experiments, feature_13,
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\rewardsFeature13.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\misallocFeature13.txt",
-                       r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\evolFeature13.txt",
-                       tit1 = "feature 13 current experiment")
-    
-    
-elif run_naive_policy:
-    reward_list = []
-    reward_evol = np.zeros(100)
-    
-    
-    # r_naives is accumulated rewards on each step
-    for i in range(1):
-        evol_temp = []
-        
-        for experiment in range(number_of_experiments):
-            #if experiment == 0:
-            plot = False
-            p_naive, r_naives, *rest = simulate(hospital_r, naive = True, steps = number_of_steps, plot = plot)
-                
-            reward_list.append(sum(r_naives))
-            evol_temp.append(sum(r_naives))
-            
-        reward_evol += np.array(evol_temp)
-    reward_evol = reward_evol / 50
-                
-           # else:
-            #    plot = False
-             #   p_naive, r_naives, *rest = simulate(hospital_r, naive = True, steps = number_of_steps, plot = plot)
-              #  
-               # reward_list.append(sum(r_naives)) # needs some fixing
-                #
-                #reward_evol.append(sum(r_naives))
-        
-    writeToFile(r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\evolNaive.txt", reward_evol)
-    #writeToFile(r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\rewardsNaive.txt", reward_list)
-    print("The list with the rewards", reward_list)
-    print("The average reward after {} experiments is {}".format(number_of_experiments, np.mean(reward_list)))
-    print("The median is {}".format(np.median(np.array(reward_list))))
-    print("The standard deviation is {}".format(np.std(np.array(reward_list))))
-    print(reward_evol)
-
-# temporary here
-if calcRewards:
-    my_list = []
-    read = open(r"C:\Users\todor\Documents\UCL_MATH_ECON\Year_3\Foundations_ML\RL_project\rewardsFeature12.txt","r")
-    for line in read:
-        my_list.append(float(line.rstrip()))
-    read.close()
-    print("The length of the list: {}\n".format(len(my_list)))
-    print("The median: {}\n".format(np.median(np.array(my_list))))
-    print("The standard deviation : {}".format(np.std(np.array(my_list))))
-
-
-
-
-
-
-
-
-
-
-
